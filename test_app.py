ca74da0489db60080d5235518d839694bc777cf5
PawelDalek <pawel.piotr.dalek@gmail.com>
2026-05-15 19:35:14 +0200

    Naprawa testow na feature branch

--- test_app.py
+++ test_app.py
@@ -1,17 +1,22 @@
 from app import dodaj, odejmij, pomnoz, podziel
+
 def test_dodaj():
-assert dodaj(2, 3) == 5
+    assert dodaj(2, 3) == 5
+
 def test_odejmij():
-assert odejmij(5, 2) == 3
+    assert odejmij(5, 2) == 3
+
 def test_pomnoz():
-assert pomnoz(2, 4) == 8
+    assert pomnoz(2, 4) == 8
+
 def test_podziel():
-assert podziel(10, 2) == 5
-assert podziel(0, 5) == 0
-assert podziel(7, 0) == "Blad: dzielenie przez zero"
+    assert podziel(10, 2) == 5
+    assert podziel(0, 5) == 0
+    assert podziel(7, 0) == "Blad: dzielenie przez zero"
+
 if __name__ == "__main__":
-test_dodaj()
-test_odejmij()
-test_pomnoz()
-test_podziel()
-print("Wszystkie testy OK")
+    test_dodaj()
+    test_odejmij()
+    test_pomnoz()
+    test_podziel()
+    print("Wszystkie testy OK")
